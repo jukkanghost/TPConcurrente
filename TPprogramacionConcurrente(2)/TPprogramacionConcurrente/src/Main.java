a61f80a5cf3ed707de4e8eab134a3b9b135f2041
jukkanghost <juli_0630@hotmail.com>
2021-02-21 19:07:37 -0300

    intento de todas las variables de regex

--- TPprogramacionConcurrente(2)/TPprogramacionConcurrente/src/Main.java
+++ TPprogramacionConcurrente(2)/TPprogramacionConcurrente/src/Main.java
@@ -60,16 +60,22 @@ public class Main {
 		//Tinvariantes
 		int [] gruposInvariante1 = {2, 6, 8, 12, 14, 16, 18, 20};
 		InvTransicion invariante1 = new InvTransicion("invariante1", gruposInvariante1);
-		int [] gruposInvariante2 = {2, 6, 8, 22, 24};
+		int [] gruposInvariante2 = {2, 6, 8, 22, 24, 26};
 		InvTransicion invariante2 = new InvTransicion("invariante2", gruposInvariante2);
-		int [] gruposInvariante3 = {2, 26, 28, 32, 34, 36, 38, 40};
+		int [] gruposInvariante3 = {2, 6, 8, 28, 30, 32};
 		InvTransicion invariante3 = new InvTransicion("invariante3", gruposInvariante3);
-		int [] gruposInvariante4 = {2, 26, 28, 42, 44};
+		int [] gruposInvariante4 = {2, 6, 8, 34, 36, 38};
 		InvTransicion invariante4 = new InvTransicion("invariante4", gruposInvariante4);
+		int [] gruposInvariante5 = {2, 40, 42, 46, 48, 50, 52, 54};
+		InvTransicion invariante5 = new InvTransicion("invariante5", gruposInvariante5);
+		int [] gruposInvariante6 = {2, 40, 42, 56, 58};
+		InvTransicion invariante6 = new InvTransicion("invariante6", gruposInvariante6);
 		listaInvariantes.add(invariante1);
 		listaInvariantes.add(invariante2);
 		listaInvariantes.add(invariante3);
 		listaInvariantes.add(invariante4);
+		listaInvariantes.add(invariante5);
+		listaInvariantes.add(invariante6);
 		Regex regex = new Regex(log, listaInvariantes);
 
 
